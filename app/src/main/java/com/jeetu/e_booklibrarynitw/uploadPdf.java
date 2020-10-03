b6a4e3c2aaf4a86b17d54f5da446b7d198bc13c2
jeetu <jitendrakumarverma126@gmail.com>
2020-10-03 16:35:37 +0530

    upload e-book

---
+++ app/src/main/java/com/jeetu/e_booklibrarynitw/uploadPdf.java
@@ -0,0 +1,26 @@
+package com.jeetu.e_booklibrarynitw;
+
+public class uploadPdf {
+    public String name;
+    public String url;
+
+    public uploadPdf() {
+    }
+
+    public uploadPdf(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+
+}
